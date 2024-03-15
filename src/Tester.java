464791afbfacefc090489acbb28023539be93dd0
BT_1N3_33 <kjashjkashkh@lasjdklha>
2024-03-15 09:29:45 -0400

    StatingCodeForMap

---
+++ src/Tester.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+public class Tester {
+    public static JFrame window;
+
+    public static void main(String[] args){
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Rpg");
+
+        GamePanel gamePanel = new GamePanel();
+
+        window.add(gamePanel);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+
+    }
+}
